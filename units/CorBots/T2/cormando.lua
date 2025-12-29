5fcc18db222ac7e2f301ccfb0dc26899de533ea1
SethDGamre <sethdgamre@gmail.com>
2025-12-29 13:28:37 -0600

    working

--- units/CorBots/T2/cormando.lua
+++ units/CorBots/T2/cormando.lua
@@ -7,6 +7,7 @@ return {
 		buildpic = "CORMANDO.DDS",
 		buildtime = 17100,
 		canassist = true,
+		canattack = true,
 		canmove = true,
 		canreclaim = true,
 		canrepair = true,
@@ -127,6 +128,9 @@ return {
 				weaponvelocity = 930,
 				damage = {
 					default = 100,
+				},
+				customparams = {
+					exclude_preaim = true,
 				},
 			},
 		},
